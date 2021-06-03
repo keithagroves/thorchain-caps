cd98aaf58aa086bf97a22aace15828de02c38fa6
keithagroves <keith.groves@jointheleague.org>
2021-06-02 21:04:32 -0700

    Update Cap.tsx

--- src/Cap.tsx
+++ src/Cap.tsx
@@ -22,7 +22,7 @@ function Cap() {
     "mimir//CHURNINTERVAL": 288000,
     "mimir//FULLIMPLOSSPROTECTIONBLOCKS": 0,
     "mimir//FUNDMIGRATIONINTERVAL": 0,
-    "mimir//MAXIMUMLIQUIDITYRUNE": 50000000000000,
+    "mimir//MAXIMUMLIQUIDITYRUNE": 0,
     "mimir//MINIMUMBONDINRUNE": 3000000000000,
     "mimir//MINRUNEPOOLDEPTH": 1000000000000,
     "mimir//OBSERVATIONDELAYFLEXIBILITY": 20,
@@ -64,7 +64,7 @@ let cap = data['mimir//MAXIMUMLIQUIDITYRUNE']/ (10 ** 8);
   return (
     <div className="App">
      
-       {cap  > 500000? <h1> Caps may have been raised! Check here <a href='https://twitter.com/THORChain'>THORChain Twitter</a> <br></br>New Cap: {cap}</h1> : <div><h1>Max Rune: {cap}</h1> <p>Do not attempt to add liquidity.</p></div>}
+       {cap  > 3700000? <h1> Caps may have been raised! Check here <a href='https://twitter.com/THORChain'>THORChain Twitter</a> <br></br>New Cap: {cap}</h1> : <div><h1>Max Rune: {cap}</h1> <p>Do not attempt to add liquidity.</p></div>}
       <LastUpdate update={date}></LastUpdate>
 
     </div>
